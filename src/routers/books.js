bef33b1c672533a2fe5a09bafc2261eb0df57f80
Akhil-EM <akhilemcalicut@gmail.com>
2021-02-11 16:17:59 +0530

    added and aggrigation in books categoy search

--- src/routers/books.js
+++ src/routers/books.js
@@ -92,7 +92,7 @@ function routerHandler(){
                 }
                 else{
 
-                    booksModel.find({genre:req.body.category},(err,result)=>{
+                    booksModel.find({$and:[{genre:req.body.category},{libraryId:req.body.libaryId}]},(err,result)=>{
                         if(err){
                             console.log(chalk.redBright(`${err} error occured`));
                             res.json({status:'error'});
